2ee30bfce7255cf7a10d93a39465c5a3a7c230cd
qqcarlosqq <qqcarlosqq@gmail.com>
2025-06-02 23:00:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -138,3 +138,7 @@ function renderTabla(idTabla, lista) {
         tbody.appendChild(fila);
     }
 }
+
+
+// -- rellenar selects cuando DOM listo --
+document.addEventListener('DOMContentLoaded', buildSelects);
